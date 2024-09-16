893cacc46e44888b06153f8ca822b2376b16d3aa
Philipp Steinkopff <p.steinkopff@techdivision.com>
2024-09-16 15:03:41 +0200

    clean up

--- blocks/image-show/image-show.js
+++ blocks/image-show/image-show.js
@@ -1,6 +1,3 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
-
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
